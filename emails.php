999158ac72a39db980bae5e74691fa8fe30b66d3
NUGGAN - NGGG <NuggaN85@users.noreply.github.com>
2016-01-04 13:57:37 +0100

    Update emails.php

--- emails.php
+++ emails.php
@@ -1,5 +1,12 @@
 <?php 
-
+/*!
+* protectionantiplagiat init v1.0
+* Dev: NuggaN85
+* Github: NuggaN85
+* Twitter: @NuggaN85
+* Copyright © 2015 All rights reserved.
+* Licensed under CC BY 3.0
+*/
 $limit = 5000; //Nombre d'emails generer
 $page = '';
 for ($i = 0; $i < $limit; $i++) {
